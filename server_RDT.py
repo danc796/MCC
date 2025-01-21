d8ee0e6821f41483d63b1fab2adc9ffc4011d685
danc796 <112656349+danc796@users.noreply.github.com>
2025-01-22 01:55:15 +0200

    Update server_RDT.py

--- server_RDT.py
+++ server_RDT.py
@@ -27,6 +27,8 @@ class RDPServer:
         self.last_image = None
         self.lock = threading.Lock()
 
+        self.shift_pressed = False
+
     def start(self):
         """Start the RDP server and listen for connections"""
         print(f"Server started on {self.host[0]}:{self.host[1]}")
@@ -101,92 +103,82 @@ class RDPServer:
             conn.close()
 
     def process_input(self, key, action, x, y):
-        """Process individual input events including keyboard"""
+        """Process individual input events"""
         try:
-            if key == 4:  # Mouse move
-                mouse.move(x, y)
+            # Update shift key state
+            if key in (42, 54):  # Left or right shift
+                self.shift_pressed = (action == 100)  # 100 for keydown, 117 for keyup
+                return
 
-            elif key == 1:  # Left click
-                if action == 100:
-                    ag.mouseDown(button=ag.LEFT)
-                elif action == 117:
-                    ag.mouseUp(button=ag.LEFT)
-
-            elif key == 2:  # Scroll
-                scroll_amount = self.SCROLL_SENSITIVITY if action else -self.SCROLL_SENSITIVITY
-                ag.scroll(scroll_amount)
-
-            elif key == 3:  # Right click
-                if action == 100:
-                    ag.mouseDown(button=ag.RIGHT)
-                elif action == 117:
-                    ag.mouseUp(button=ag.RIGHT)
-
-            else:  # Keyboard input
+            # Handle keyboard input
+            if key < 200:  # Not a mouse event
                 try:
                     if action == 100:  # Key down
-                        ag.keyDown(self._scan_to_key(key))
+                        ag.keyDown(self._scan_to_key(key, self.shift_pressed))
                     elif action == 117:  # Key up
-                        ag.keyUp(self._scan_to_key(key))
+                        ag.keyUp(self._scan_to_key(key, self.shift_pressed))
                 except KeyError:
                     print(f"Unrecognized scan code: {key}")
+            else:
+                # Handle mouse events as before
+                if key == self.MOUSE_MOVE:
+                    mouse.move(x, y)
+                elif key == self.MOUSE_LEFT:
+                    if action == 100:
+                        ag.mouseDown(button=ag.LEFT)
+                    elif action == 117:
+                        ag.mouseUp(button=ag.LEFT)
+                elif key == self.MOUSE_SCROLL:
+                    scroll_amount = self.SCROLL_SENSITIVITY if action else -self.SCROLL_SENSITIVITY
+                    ag.scroll(scroll_amount)
+                elif key == self.MOUSE_RIGHT:
+                    if action == 100:
+                        ag.mouseDown(button=ag.RIGHT)
+                    elif action == 117:
+                        ag.mouseUp(button=ag.RIGHT)
 
         except Exception as e:
             print(f"Input processing error: {e}")
 
-    def _scan_to_key(self, scan_code):
-        """Convert scan code to PyAutoGUI key name"""
-        # Common scan code to key mappings
+    def _scan_to_key(self, scan_code, shift_pressed):
+        """Convert scan code to PyAutoGUI key name, considering shift state"""
         scan_map = {
-            # Letters
             30: 'a', 48: 'b', 46: 'c', 32: 'd', 18: 'e', 33: 'f', 34: 'g', 35: 'h',
             23: 'i', 36: 'j', 37: 'k', 38: 'l', 50: 'm', 49: 'n', 24: 'o', 25: 'p',
             16: 'q', 19: 'r', 31: 's', 20: 't', 22: 'u', 47: 'v', 17: 'w', 45: 'x',
-            21: 'y', 44: 'z',
-
-            # Numbers (main keyboard)
-            2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7', 9: '8', 10: '9',
-            11: '0',
-
-            # Special keys
-            28: 'enter',
-            1: 'esc',
-            14: 'backspace',
-            15: 'tab',
-            57: 'space',
-            42: 'shift',
-            54: 'rshift',  # Right shift
-            29: 'ctrl',
-            56: 'alt',
-
-            # Arrow keys
-            72: 'up',
-            80: 'down',
-            75: 'left',
-            77: 'right',
-
-            # Function keys
-            59: 'f1',
-            60: 'f2',
-            61: 'f3',
-            62: 'f4',
-            63: 'f5',
-            64: 'f6',
-            65: 'f7',
-            66: 'f8',
-            67: 'f9',
-            68: 'f10',
-            87: 'f11',
-            88: 'f12',
-
-            # Additional keys
-            55: '/',
-            74: '-',
-            78: '+',
-            83: 'del'
+            21: 'y', 44: 'z', 2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7',
+            9: '8', 10: '9', 11: '0', 26: '[', 27: ']', 43: '\\', 39: ';', 40: "'",
+            41: '`', 51: ',', 52: '.', 53: '/', 12: '-', 13: '=', 28: 'enter',
+            1: 'esc', 14: 'backspace', 15: 'tab', 57: 'space', 42: 'shift',
+            54: 'rshift', 29: 'ctrl', 56: 'alt', 72: 'up', 80: 'down', 75: 'left',
+            77: 'right', 59: 'f1', 60: 'f2', 61: 'f3', 62: 'f4', 63: 'f5', 64: 'f6',
+            65: 'f7', 66: 'f8', 67: 'f9', 68: 'f10', 87: 'f11', 88: 'f12', 83: 'delete',
+            71: 'home', 79: 'end', 81: 'pagedown', 73: 'pageup', 55: '*', 74: '-',
+            78: '+'
         }
 
-        return scan_map[scan_code]
+        # Define shifted keys dynamically
+        shifted_map = {
+            '1': '!', '2': '@', '3': '#', '4': '$', '5': '%', '6': '^', '7': '&',
+            '8': '*', '9': '(', '0': ')', '[': '{', ']': '}', '\\': '|', ';': ':',
+            "'": '"', '`': '~', ',': '<', '.': '>', '/': '?', '-': '_', '=': '+'
+        }
+
+        # Get the base key
+        key = scan_map.get(scan_code)
+        if not key:
+            return None
+
+        # Return shifted key if Shift is pressed and a shifted variant exists
+        if shift_pressed and key in shifted_map:
+            return shifted_map[key]
+
+        return key
+
+    MOUSE_LEFT = 201
+    MOUSE_SCROLL = 202
+    MOUSE_RIGHT = 203
+    MOUSE_MOVE = 204
 
 
 if __name__ == '__main__':
